efacaace3b060b9e33f579502ac5e3ec996f394b
Prince Donkor <prince7475>
2018-09-15 13:13:02 -0400

    about me

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Nav from './Nav'
 import Slides from './Slides'
+import About from './About'
 export default class Main extends Component {
   render() {
     return (
       <div>
         <Nav/>
         <Slides/>
+        <About />
       </div>
     )
   }
